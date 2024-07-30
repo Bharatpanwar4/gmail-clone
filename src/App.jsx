d2258572acc8566b15ac5cfbad4829d0bb3e8eca
bharatpanwar4 <bharatpanwar4@gmail.com>
2024-07-30 09:47:30 +0530

    created project with vite and installed tailwind

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Header from './components/common/Header'
+
+function App() {
+
+  return (
+    <>
+  <div className='flex items-center text-2xl text-red-200'>
+    hahahah
+    <Header/>
+  </div>
+    </>
+  )
+}
+
+export default App
